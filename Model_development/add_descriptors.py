c2ddbb3af1b3add5b6ab2eb2e1093941df7b43aa
ZHAOHAN ZHANG <44449774+Hannahzz@users.noreply.github.com>
2020-07-31 13:54:34 -0500

    Create add_descriptors.py

---
+++ Model_development/add_descriptors.py
@@ -0,0 +1,36 @@
+## Add numerical descriptors via Matminer
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matminer.utils.conversions import str_to_composition
+from matminer.featurizers.composition import ElementProperty
+
+##check for missing values
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+
+df_binary = pd.read_csv('../Datasets/Binary_intermetallics.csv',)
