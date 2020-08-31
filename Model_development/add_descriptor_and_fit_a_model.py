d99b781d48c1bed4c6fecd2fa3b77ab31624ee98
ZHAOHAN ZHANG <44449774+Hannahzz@users.noreply.github.com>
2020-08-30 19:02:09 -0500

    Update and rename add_descriptors.py to add_descriptor_and_fit_a_model.py

--- Model_development/add_descriptor_and_fit_a_model.py
+++ Model_development/add_descriptor_and_fit_a_model.py
@@ -56,3 +56,64 @@ df_el = ep_feat_1.featurize_dataframe(df_el, col_id="composition")  # input the 
 ##check for missing value
 missing_values_table(df_binary)
 missing_values_table(df_el)
+
+##Allign data and normalization
+excluded = ['pretty_formula','formation_energy_per_atom','composition']
+X = df_binary.drop(excluded, axis=1)
+X_el = df_el.drop(excluded, axis=1)
+Y = df_binary.filter(items=['formation_energy_per_atom'],axis=1)
+Y_el= df_el.filter(items=['formation_energy_per_atom'],axis=1)
+
+from mlxtend.preprocessing import minmax_scaling
+X_n = minmax_scaling(X, columns=X.columns)
+X_el_n = minmax_scaling(X_el, columns=X_el.columns)
+
+## Plot Pearson correlation
+import seaborn as sns
+df_corr = pd.concat([df_binary['formation_energy_per_atom'], X], axis = 1)
+df_corr = df_corr.corr()
+plt.figure(figsize = (6, 6) )
+sns.heatmap(df_corr, cmap='RdYlBu', vmin = -0.8, vmax = 0.8)
+plt.title('Heatmap', fontsize = 20)
+
+## Train_test_split
+from sklearn.model_selection import train_test_split
+##use 20% of binary as testing set
+X_1, X_2, Y_1, Y_2 = train_test_split(X_n, Y, test_size=0.2, random_state=2)
+X_t=pd.concat([X_1, X_el_n])
+Y_t=pd.concat([Y_1, Y_el]) ##add metallic elements as training data
+
+##10-fold CV with GPR
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel, RationalQuadratic, ExpSineSquared, Matern
+from sklearn.model_selection import cross_val_predict, KFold
+kernel = 1.0*WhiteKernel()+1.0*RBF() 
+gpr = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=5,
+                              optimizer= 'fmin_l_bfgs_b', copy_X_train=True, normalize_y=True)
+kf = KFold(n_splits=10,shuffle=True, random_state=12345)
+for train_index, test_index in kf.split(X_t):
+    X_train, X_test = X_t.iloc[train_index], X_t.iloc[test_index]
+    Y_train, Y_test = Y_t.iloc[train_index], Y_t.iloc[test_index]
+    gpr.fit(X_train,Y_train)
+    gpr.score(X_train,Y_train) 
+    gpr.score(X_test,Y_test)
+
+def rmse (predictions, targets):
+    return np.sqrt(((predictions-targets)**2).mean())
+def MAE (predictions, targets):
+    return np.mean(abs(predictions-targets))
+
+
+##training set accuracy
+predicted_1=gp_try.predict(X_1)
+pred = np.array(predicted_1)
+Y_1_array=np.array(Y_1)
+print("root mean square error: %.3f" %(rmse(pred, Y_1_array)))
+print("mean abosolute error: %.3f" %(MAE(pred, Y_1_array)))
+
+##testing set accuracy
+predicted_2=gp_try.predict(X_2)
+pred_2 = np.array(predicted_2)
+Y_2_array=np.array(Y_2)
+print("root mean square error: %.3f" %(rmse(pred_2, Y_2_array)))
+print("mean abosolute error: %.3f" %(MAE(pred_2, Y_2_array)))
